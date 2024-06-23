91906d20f7d771bd93ee639e78da62d0209c2847
Isabela Nogueira <isabela.nogueira@liferay.com>
2024-06-23 16:08:51 -0300

    Components

---
+++ my-movies/src/components/MainMenu.js
@@ -0,0 +1,10 @@
+import { MainMenuContainer } from "../style/MainMenuContainer";
+
+export const MainMenu = () => (
+  <MainMenuContainer>
+    <ul>
+      <li>Home</li>
+      <li>Sobre</li>
+    </ul>
+  </MainMenuContainer>
+);
